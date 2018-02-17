e7c2b124d8f0b5a9dacadd2cd7807d1453e44667
Jacob Wernerowski <jacob.wernerowski@mail.utoronto.ca>
2018-02-17 12:43:16 -0500

    Bug fixes in pie chart method

--- NutritionGraphics.py
+++ NutritionGraphics.py
@@ -1,28 +1,35 @@
 import matplotlib.pyplot as plt
 import os
-import Food
+from Food import *
+
 
 
 class NutritionGraphics:
 
-    def __init__(self, food: Food) -> None:
+    def __init__(self, food) -> None:
         """
         Initialize a NutrionGraphics object for a single food object.
         """
         self.food = food
         # Gets unique nutrients that are measured in grams.
         # Works roughly for liquids since 1 mL of liquids ~ 1 g
-        self.relevant_nutrients = [food.nutrients[key] for key in food.nutrients
-                                   if key not in ("calories", "cholestrol",
-                                                  "sodium", "carbohydrate")]
+        self.relevant_nutrients = [nutrient for nutrient in food.nutrients
+                                   if nutrient not in ("calories", "cholestrol",
+                                                  "sodium", "carbohydrate", "fat")]
+        self.relevant_nutrients.append("unsaturated fat")
 
-    def create_nutrition_pie_chart(self) -> None:
+    def create_nutrition_pie_chart(self):
         """
         Saves a pie chart of nutrition for a single food object.
         """
         labels = self.relevant_nutrients
         sizes = [(self.food.nutrients[key] / self.food.weight) * 100 for
-                 key in labels]
+                 key in labels if key != "unsaturated fat"]
+        # Find unsaturated fat amount. Total fat - saturated fat.
+        sizes.append(self.food.nutrients["fat"] - self.food.nutrients["saturatedfat"])
+        # Replace saturatedfat with saturated fat.
+        labels[labels.index("saturatedfat")] = "saturated fat"
+
         explode = [0.05]*len(labels)
 
         fig1, ax1 = plt.subplots()
@@ -32,13 +39,13 @@ class NutritionGraphics:
 
         plt.title(f"{self.food.name} nutrition breakdown (grams)")
         plt.savefig(self.food.name)
+        return self.food.name
 
     def delete_image_files(self):
         """Deletes images created once no longer needed."""
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        dir = os.listdir(dir_path)
+        get_dir = os.listdir(dir_path)
 
-        for item in dir:
+        for item in get_dir:
             if item.endswith(".png"):
                 os.remove(os.path.join(dir_path, item))
-
