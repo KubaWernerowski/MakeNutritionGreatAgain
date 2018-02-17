908570a3f0d81f66ec10631c4ecdde7af3d218af
Jacob Wernerowski <jacob.wernerowski@mail.utoronto.ca>
2018-02-17 14:04:49 -0500

    maybe??? finished bar graph code

--- NutritionGraphics.py
+++ NutritionGraphics.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import os
 import numpy as np
 
@@ -47,10 +48,53 @@ class NutritionGraphics:
         """
         Creates a bar graph comparing two distinct foods' nutrients.
         """
+        ORDERED_NUTRIENTS = [x for x in self.food.nutrients]
 
+        food1data = [[self.food.name, nutrient,
+                      self.food.nutrients[nutrient]] for nutrient in ORDERED_NUTRIENTS]
+        food2data = [[other_food.food.name, nutrient,
+                      other_food.food.nutrients[nutrient]] for nutrient in ORDERED_NUTRIENTS]
+
+        dpoints = np.array(food1data, food2data)
+
+        bar = plt.figure()
+        ax = bar.add_subplot(111)
+
+        space = 0.3
+
+        conditions = np.unique(dpoints[:, 0])
+        categories = np.unique(dpoints[:, 1])
+
+        n = len(conditions)
+
+        width = (1 - space) / len(conditions)
+
+        print("width:", width)
+
+        for i, cond in enumerate(conditions):
+            vals = dpoints[dpoints[:, 0] == cond][:, 2].astype(np.float)
+            pos = [j - (1 - space) / 2. + i * width for j in range(1, len(categories) + 1)]
+
+            ax.bar(pos, vals, width=width)
+
+            ax.set_xticks("indeces")
+            ax.set_xticklabels(categories)
+            plt.setp(plt.xticks()[1], rotation=90)
+
+            ax.set_ylabel("Amount")
+            ax.set_xlabel("Nutrient")
+
+            ax.bar(pos, vals, width=width, label=cond, color=cm.Accent(float(i) / n))
+
+            handles, labels = ax.get_legend_handles_labels()
+            ax.legend(handles[::-1], labels[::-1])
+
+
+            bar_graph = self.food.name + "_" + other_food.food.name + "_bar_graph"
+            plt.savefig(bar_graph)
 
         self.bar_graph = "./" + self.food.name + "_" + other_food.food.name + "_bar_graph.png"
-        
+
 
 
     def delete_image_files(self):
