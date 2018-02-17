3322426a3e7033ce4f7fce0c578234f6af2b63b1
Jacob Wernerowski <jacob.wernerowski@mail.utoronto.ca>
2018-02-17 11:22:06 -0500

    Created rudimentary pie chart break down function of food object nutrition

--- NutritionGraphics.py
+++ NutritionGraphics.py
@@ -1,30 +1,37 @@
 import matplotlib.pyplot as plt
 import os
+import Food
 
 
 class NutritionGraphics:
 
-    def __init__(self, food):
+    def __init__(self, food: Food) -> None:
         """
         Initialize a NutrionGraphics object for a single food object.
         """
         self.food = food
+        # Gets unique nutrients that are measured in grams.
+        # Works roughly for liquids since 1 mL of liquids ~ 1 g
+        self.relevant_nutrients = [food.nutrients[key] for key in food.nutrients
+                                   if key not in ("calories", "cholestrol",
+                                                  "sodium", "carbohydrate")]
 
-    def create_nutrition_pie_chart(self):
+    def create_nutrition_pie_chart(self) -> None:
         """
-        Returns a pie chart of nutrition for a single food object.
+        Saves a pie chart of nutrition for a single food object.
         """
-        labels = ['Frogs', 'Hogs', 'Dogs', 'Logs']
-        sizes = [15, 30, 45, 10]
-
-        explode = (0.05, 0.05, 0.05, 0.05)
+        labels = self.relevant_nutrients
+        sizes = [(self.food.nutrients[key] / self.food.weight) * 100 for
+                 key in labels]
+        explode = [0.05]*len(labels)
 
         fig1, ax1 = plt.subplots()
         ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
                 shadow=True, startangle=90)
         ax1.axis('equal')
 
-        plt.savefig("food_name")
+        plt.title(f"{self.food.name} nutrition breakdown (grams)")
+        plt.savefig(self.food.name)
 
     def delete_image_files(self):
         """Deletes images created once no longer needed."""
@@ -35,9 +42,3 @@ class NutritionGraphics:
             if item.endswith(".png"):
                 os.remove(os.path.join(dir_path, item))
 
-
-
-
-
-a = NutritionGraphics("hello")
-a.delete_image_files()
